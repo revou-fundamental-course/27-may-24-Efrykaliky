b7794ebc400932a6164244b3d7ba0928ff777ba6
Efry Kaliky <efrykaliky1997@gmail.com>
2024-06-06 18:36:06 +0700

    update

--- js/script.js
+++ js/script.js
@@ -12,29 +12,33 @@ const navSlide = () => {
 navSlide();
 
 //form
-function inputForm() {
-  const nameInput = document.forms["myform"]["name"].value;
-  const dateInput = document.forms["myform"]["date"].value;
-  const genderInput = document.forms["myform"]["gender"].value;
-  const messageInput = document.forms["myform"]["message"].value;
+document.getElementById('formulir').addEventListener('submit', function(event) {
+  event.preventDefault();  // Mencegah pengiriman formulir default
 
-  if (
-    nameInput == "" ||
-    dateInput == "" ||
-    genderInput == "" ||
-    messageInput == ""
-  ) {
-    alert("Data tidak boleh ada yang kosong");
-    return false;
-  }
 
-  setSenderUI(nameInput, dateInput, genderInput, messageInput);
-  return false;
-}
+  //membuat tanggal sekarang
+  const now = new Date();
+  const dateTimeString = now.toLocaleString();
+ 
 
-function setSenderUI(nameInput, dateInput, genderInput, messageInput) {
-  document.getElementById("sender-name").innerHTML = nameInput;
-  document.getElementById("sender-date").innerHTML = dateInput;
-  document.getElementById("sender-gender").innerHTML = genderInput;
-  document.getElementById("sender-message").innerHTML = messageInput;
-}
+  // Ambil nilai dari formulir
+ 
+  const nama = document.getElementById('nama').value;
+  const tglLahir = document.getElementById('tgl_lahir').value;
+  const jenisKelamin = document.querySelector('input[name="jenis_kelamin"]:checked').value;
+  const pesan = document.getElementById('pesan').value;
+
+  // Buat teks hasil inputan
+  const resultText = `Date/time: ${dateTimeString}\n \nName: ${nama}\nDate of Birth: ${tglLahir}\nGender: ${jenisKelamin}\nMessages:\n${pesan}`;
+
+  // Tampilkan hasil inputan di text area
+  document.getElementById('result').value = resultText;
+
+  // Tampilkan pesan alert
+  alert('Your Message Has Been Submitted');
+  document.getElementById('nama').value ="";
+  document.getElementById('tgl_lahir').value ="";
+  document.getElementById('pesan').value ="";
+  
+});
+  
